5700b1fd5435883546500c48d2d46b03bf091142
giohuram <giovanni.masala92@gmail.com>
2024-09-18 19:20:56 +0100

    add dotenv example file

--- backend/Routes/tutor.js
+++ backend/Routes/tutor.js
@@ -9,6 +9,7 @@ const router = express.Router()
 
 // nested route 
 router.use("/:tutorId/reviews", reviewRouter); 
+
 router.get('/:id', getSingleTutor); 
 router.get('/', getAllTutor); 
 router.put('/:id', authenticate, restrict(["tutor"]),  updateTutor); 
